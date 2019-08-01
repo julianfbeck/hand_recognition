68c904be362205a84d7a1c2834f40d5c25a6b703
Julian Beck <ju-fa-beck@t-online.de>
2019-08-01 13:53:18 +0200

    update filenames

--- object_detection_multithreading.py
+++ object_detection_multithreading.py
@@ -15,11 +15,10 @@ from object_detection.utils import label_map_util
 CWD_PATH = os.getcwd()
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-PATH_TO_CKPT = os.path.join(CWD_PATH, 'object_detection', MODEL_NAME, 'frozen_inference_graph.pb')
+PATH_TO_CKPT = os.path.join(CWD_PATH, 'object_detection', 'model', 'frozen_inference_graph.pb')
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join(CWD_PATH, 'object_detection', 'data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS = os.path.join(CWD_PATH, 'object_detection', 'label', 'label_map.pbtxt')
 
 NUM_CLASSES = 11
 
